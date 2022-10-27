99264f55a56b5420ca40a18c1e968961552e0924
01792166684 <tanzinatania491991@gmail.com>
2022-10-27 16:36:54 +0600

    pdf added

--- src/pages/CourseDetails.js
+++ src/pages/CourseDetails.js
@@ -1,29 +1,30 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
-import ReactToPrint from 'react-to-print';
+import Pdf from "react-to-pdf";
 
 
 const CourseDetails = () => {
     const courseDetails = useLoaderData();
-    const componentRef = useRef();
-    const{title ,image,details,duration,cost,time,id} = courseDetails
+    const ref = React.createRef();
+    const{title,about ,image,details,duration,cost,time,id} = courseDetails
     return (
-    
-        <div>
+      
+        <div className='text-left'>
           <div className='text-center my-2'>
-          <ReactToPrint 
-        trigger={() => <Button   variant="danger">Print This Out</Button>}
-        content={() => componentRef.current}
-      />
+          <Pdf targetRef={ref} filename="code-example.pdf">
+        {({ toPdf }) => <Button onClick={toPdf}  variant="danger">Generate pdf</Button>}
+      </Pdf>
           </div>
-          <div ref={componentRef} className="card m-5" style={{height:'500px'}} >
-        <div className="row g-0">
+          <div ref={ref} className="card mx-auto" style={{ width:'400px',height: '400px'}} >
+            <p className='p-3 text-secondary '>{about}</p>
+            </div>
+        <div className="row m-4 g-0">
           <div className="col-md-4">
             <img src={image} style={{height:'500px'}} className="img-fluid rounded-start" alt="..."/>
           </div>
           <div className="col-md-8">
-            <div className="card-body">
+            <div className=" mx-4 card-body">
               <h5 className="card-title text-secondary">{title}</h5>
               <p className="card-text text-secondary">{details}</p>
               <div className='d-flex justify-content-evenly'>
@@ -32,13 +33,13 @@ const CourseDetails = () => {
             <p className="card-text text-dark fw-bold">{time}</p>
               </div>
           <div className='text-center m-4'>
-            <Link to={`/checkout/${id}`}> <Button   variant="danger">Get premium course</Button>{' '}</Link>
+            <Link to={`/checkout/${id}`}> <Button   variant="danger">Get premium access</Button>{' '}</Link>
           </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
+      
     );
 };
 
